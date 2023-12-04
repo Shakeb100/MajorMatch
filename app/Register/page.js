a123dca35f33669f6277fd516ab187d7cec2b669
Cameron Burke <cfb5473@psu.edu>
2023-12-03 20:09:55 -0500

    Organized text boxes to be stacked on each other, for styling.

--- app/Register/page.js
+++ app/Register/page.js
@@ -18,8 +18,8 @@ export default function Register() {
     //Added register heading with following size parameters and location on webpage
 
     //Next Creating buttons for the following parameters above, being username, name, email, and password
-    <div style={{width: '100%', maxWidth: '400', margin: '0' }}>
-      <h1 style={{ fontSize: '24', textAlign: 'center', marginBottom: '10'}}>Register</h1>
+    <div style={{width: '100%', maxWidth: '100px', margin: '0' }}>
+      <h1 style={{ fontSize: '24px', textAlign: 'center', marginBottom: '10px'}}>Register</h1>
       <form>
         <input type='text' name='username' value={formData.username} onChange={handleChange} placeholder='Username' />
         <input type='text' name='name' value={formData.name} onChange={handleChange} placeholder='Name' />
