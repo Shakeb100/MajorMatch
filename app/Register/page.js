882d80f7de694aef08aaa302027d249f12470eff
Cameron Burke <cfb5473@psu.edu>
2023-12-03 19:56:40 -0500

    Starting the creation of buttons on the register page

--- app/Register/page.js
+++ app/Register/page.js
@@ -15,9 +15,26 @@ export default function Register() {
   };
 
   return(
+    //Added register heading with following size parameters and location on webpage
+
+    //Next Creating buttons for the following parameters above, being username, name, email, and password
     <div style={{width: '100%', maxWidth: '400', margin: '0' }}>
       <h1 style={{ fontSize: '24', textAlign: 'center', marginBottom: '10'}}>Register</h1>
+      <form>
+      <input type='text' name='username' value={formData.username} onChange={handleChange} placeholder='Username' />
+      <input type='text' name='name' value={formData.username} onChange={handleChange} placeholder='Username' />
+      <input type='text' name='email' value={formData.username} onChange={handleChange} placeholder='Username' />
+      <input type='text' name='password' value={formData.username} onChange={handleChange} placeholder='Username' />
+
+
+
+
+
+
+
+      </form>
     </div>
+
   )
 
 }
