d67d8c5a6cf103d310f47666e93595afdc4d0fc8
Cameron Burke <cfb5473@psu.edu>
2023-12-03 20:19:50 -0500

    Added another div to wrap all objects defined and align them in the center of the page

--- app/Register/page.js
+++ app/Register/page.js
@@ -18,16 +18,21 @@ export default function Register() {
     //Added register heading with following size parameters and location on webpage
 
     //Next Creating buttons for the following parameters above, being username, name, email, and password
-    <div style={{width: '100%', maxWidth: '100px', margin: '0' }}>
-      <h1 style={{ fontSize: '24px', textAlign: 'center', marginBottom: '10px'}}>Register</h1>
-      <form>
-        <input type='text' name='username' value={formData.username} onChange={handleChange} placeholder='Username' />
-        <input type='text' name='name' value={formData.name} onChange={handleChange} placeholder='Name' />
-        <input type='text' name='email' value={formData.email} onChange={handleChange} placeholder='Email' />
-        <input type='text' name='password' value={formData.password} onChange={handleChange} placeholder='Password' />
 
-        <button type='submit'>Register</button>
-      </form>
+    //Wrapped the following div code in another div to center all objects being displayed on the page.
+    //The div alligns all items defined along the center axis of the page for specifics 
+    <div style={{display: 'flex', justifyContent: 'center'}}>
+      <div style={{width: '100%', maxWidth: '100px', margin: '0' }}>
+        <h1 style={{ fontSize: '24px', textAlign: 'center', marginBottom: '10px'}}>Register</h1>
+        <form>
+          <input type='text' name='username' value={formData.username} onChange={handleChange} placeholder='Username' />
+          <input type='text' name='name' value={formData.name} onChange={handleChange} placeholder='Name' />
+          <input type='text' name='email' value={formData.email} onChange={handleChange} placeholder='Email' />
+          <input type='text' name='password' value={formData.password} onChange={handleChange} placeholder='Password' />
+
+          <button type='submit'>Register</button>
+        </form>
+      </div>
     </div>
   )
 }
