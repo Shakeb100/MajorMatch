ad80f6bfd438c4b0ff9bf2aade7b8dd43c813d25
Cameron Burke <cfb5473@psu.edu>
2023-12-11 16:44:54 -0500

    Finished using skeleton code from register to generate page contents

--- pages/profile.js
+++ pages/profile.js
@@ -11,7 +11,7 @@ export default function Profile() {
 
     //usage of useEffect hook which is used to throw error in website console on some action done.
     useEffect(() => {
-        fetch('/api/profile') //Make sure the endpoint matches your API route
+        fetch('/api/profile') //Make sure the file path matches your API route path
           .then(response => {
             if (!response.ok) {
               throw new Error(`HTTP error: Status ${response.status}`);
@@ -25,4 +25,28 @@ export default function Profile() {
             console.error('Error fetching user data:', error);
           });
       }, []);
+
+
+      const navigateToLogin = () => {
+        router.push('/login'); // Redirect to the login page
+      };
+    
+      return (
+        //Created div and used skeleton code similar to register to show text, textboxes and other objects on the page.
+        //Used next.js tools to align objects per our desired design
+        <div>
+          <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '20px' }}>
+            <h1>{userName || 'Loading...'}</h1>
+            <button onClick={navigateToLogin} style={{ padding: '10px', cursor: 'pointer' }}>Logout</button></div>
+          <label htmlFor="description" style={{ fontSize: 'large' }}>Description:</label>
+          <textarea
+            id="description"
+            name="description"
+            placeholder="Enter a description..."
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            style={{ width: '100%', height: '200px', marginTop: '10px' }}
+          />
+        </div>
+      );
 }
